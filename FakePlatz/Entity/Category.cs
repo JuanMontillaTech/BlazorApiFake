db46956b12cdcf44fe3a689e238eb2ec64b9b18f
Juan Ramon Montilla Sanchez <ing.juan.montilla@gmail.com>
2024-02-23 16:17:46 -0400

    first commit

---
+++ FakePlatz/Entity/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FakePlatz.Entity
+{
+	public class Category
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
+
---
+++ FakePlatz/Entity/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FakePlatz.Entity
+{
+	public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public List<string> Images { get; set; }
+    }
+}
+
